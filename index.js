cf35a97f3f95169d0d502fd06dc99337eee9b332
silvio-hf <69448369+silvio-hf@users.noreply.github.com>
2024-03-28 21:09:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 3000 });
+
+let nextPlayerId = 1;
+const connectedPlayers = {};
+
+server.on('connection', (socket) => {
+  const playerId = nextPlayerId++;
+  connectedPlayers[playerId] = socket;
+
+  console.log(`Player ${playerId} connected`);
+  // Send the player ID to the client
+  socket.send(`Your Player id is ${playerId}`);
+
+  // Handle messages from this client
+  socket.on('message', (message) => {
+    console.log(`Received from Player ${playerId}: ${message}`);
+
+    // Broadcast the message to all connected players
+    Object.values(connectedPlayers).forEach((playerSocket) => {
+      playerSocket.send(`Player ${playerId} says: ${message}`);
+    });
+  });
+
+  // Remove the player from the list when they disconnect
+  socket.on('close', () => {
+    console.log(`Player ${playerId} disconnected`);
+    delete connectedPlayers[playerId];
+    nextPlayerId--; 
+  });
+});
